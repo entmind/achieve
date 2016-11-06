4c3967489b038a745e0e247d900227514098ee94
MASA <MASA@MASA-no-MacBook-Air.local>
2016-11-06 20:52:56 +0900

    dive19_2kadai2_adjust-comment-destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,25 +4,27 @@ class CommentsController < ApplicationController
     # ログインユーザーに紐付けてインスタンス生成するため、buildメソッドを使用使用します。
     @comment = current_user.comments.build(comment_params)
     @blog = @comment.blog
-    @notification = @comment.notification.build(user_id: @blog.user.id)
+    @notification = @comment.notifications.build(user_id: @blog.user.id)
     
     # クライアント要求に応じて、フォーマットを変更します。
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました'}
-        format.json { render :show, status: :created, location: @comment }
-        # js形式でレスポンスを返します。
-        format.js { render :index }
         # dive19_2コメント機能ですよ。
         unless @comment.blog.user_id == current_user.id
           Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'comment_created', { message: 'あなたの作成したブログにコメントが付きました。' })
         end
         Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'notification_created', {
-          uncreated_count: notification.where(user_id: @comment.blog.user.id).count
+          uncreated_count: Notification.where(user_id: @comment.blog.user.id).count
           })
+        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました'}
+        format.json { render :show, status: :created, location: @comment }
+        # js形式でレスポンスを返します。
+        format.js { render :index }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
+        # js形式でレスポンスを返します。
+        format.js { render :index }
       end
     end
   end
